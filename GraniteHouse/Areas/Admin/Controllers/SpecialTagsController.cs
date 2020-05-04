cc1ec155d7453af620e42de1a8fe60a127864cec
s.mohd <sukraatmohd93@gmail.com>
2020-05-05 00:20:33 +0530

    17> Special Tags- Create Model class, Add to DB, Controller- Index, Create Action Method & Views

---
+++ GraniteHouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SpecialTagsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            
+            return View(_db.SpecialTags.ToList());
+        }
+
+        //Get Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Create Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(specialTags);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(specialTags);
+
+        }
+    }
+}
