fb21d7b68582d68f6624b93e01d6ffe3d0ac5960
s.mohd <sukraatmohd93@gmail.com>
2020-05-06 12:06:16 +0530

    34> Shopping Cart- Add Model [Appointments | ProductsSeletedForAppointments] & ViewModel [ShoppingCartViewModel] & Add to the Db.

--- GraniteHouse/Data/ApplicationDbContext.cs
+++ GraniteHouse/Data/ApplicationDbContext.cs
@@ -17,6 +17,9 @@ namespace GraniteHouse.Data
         public DbSet<ProductTypes> ProductTypes { get; set; }
         public DbSet<SpecialTags> SpecialTags { get; set; }
         public DbSet<Products> Products { get; set; }
+        public DbSet<Appointments> Appointments { get; set; }
+        public DbSet<ProductsSeletedForAppointment> ProductsSeletedForAppointments { get; set; }
+
 
     }
 }
